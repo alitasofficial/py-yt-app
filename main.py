66975f8034fb8a3149685b966ee58c6bc0ebf097
Ali Taş <alitas2400@gmail.com>
2023-01-14 12:33:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pytube
+import os
 
 st.set_page_config(page_title="Video Downloader",
                    page_icon=":film_projector:", layout="wide")
@@ -16,8 +17,12 @@ if video_url:
                                  i.resolution for i in available_qualities])
     selected_video = next(
         x for x in available_qualities if x.resolution == video_quality)
-    st.write("Downloading...")
-    selected_video.download()
-    st.success("Video downloaded!")
+    file_name = selected_video.default_filename
+    if st.button("Download"):
+        st.write("Downloading...")
+        selected_video.download()
+        st.success("Video downloaded!")
+        os.remove(file_name)
+        st.success("Video removed from server!")
 else:
     st.write("Please enter a video URL")
