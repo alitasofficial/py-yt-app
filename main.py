2c0ee91c8b9df6274e2d6e95fae52fece07740d9
Ali Taş <alitas2400@gmail.com>
2023-01-14 14:53:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,25 @@ st.set_page_config(page_title="Video Downloader",
 
 st.title("Video Downloader")
 
-with st.form('my_form'):
-    url = st.text_input("Enter the video URL")
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        yt = pytube.YouTube(url)
-        highest_resolution = yt.streams.filter(
-            progressive=True,
-            file_extension='mp4').order_by('resolution').desc().first()
-        file_name = highest_resolution.default_filename
-        try:
-            video = highest_resolution.download()
-            with open(video, 'rb') as file:
-                st.download_button(label='Download the video',
-                                   data=file,
-                                   file_name=file_name,
-                                   mime='video/mp4')
-                st.success("Video is ready!")
-                os.remove(file_name)
-        except:
-            st.write('error')
+form = st.form(key='my-form', clear_on_submit=True)
+url = form.text_input("Enter the video URL")
+submit = form.form_submit_button('Submit')
+
+
+if submit:
+    yt = pytube.YouTube(url)
+    highest_resolution = yt.streams.filter(
+        progressive=True,
+        file_extension='mp4').order_by('resolution').desc().first()
+    file_name = highest_resolution.default_filename
+    try:
+        video = highest_resolution.download()
+        with open(video, 'rb') as file:
+            st.success("Video is ready!")
+            st.download_button(label='Download the video',
+                               data=file,
+                               file_name=file_name,
+                               mime='video/mp4')
+            os.remove(file_name)
+    except:
+        st.write('error')
