b0c6ba4ea9f5fa3a3a71da4da68a9f9376044fce
Ali Taş <alitas2400@gmail.com>
2023-01-14 12:49:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pytube
 import os
 
 st.set_page_config(page_title="Video Downloader",
-                   page_icon=":film_projector:", layout="wide")
+                   page_icon=":film_projector:",
+                   layout="wide"
+                   )
 
 st.title("Video Downloader")
 
@@ -11,18 +13,22 @@ video_url = st.text_input("Enter the video URL")
 
 if video_url:
     yt = pytube.YouTube(video_url)
-    available_qualities = yt.streams.filter(
-        progressive=True, file_extension='mp4').order_by('resolution').desc().all()
-    video_quality = st.selectbox("Select video quality", [
-                                 i.resolution for i in available_qualities])
-    selected_video = next(
-        x for x in available_qualities if x.resolution == video_quality)
-    file_name = selected_video.default_filename
-    if st.button("Download"):
-        st.write("Downloading...")
-        selected_video.download()
-        st.success("Video downloaded!")
-        os.remove(file_name)
-        st.success("Video removed from server!")
-else:
-    st.write("Please enter a video URL")
+    highest_resolution = yt.streams.filter(
+        progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    file_name = highest_resolution.default_filename
+
+    try:
+        video = highest_resolution.download()
+        with open(video, 'rb') as file:
+            st.download_button(
+                label='Download the video',
+                data=file,
+                file_name=file_name,
+                mime='video/mp4'
+            )
+            st.write("Downloading...")
+            st.success("Video downloaded!")
+            os.remove(file_name)
+            st.success("Video removed from server!")
+    except:
+        st.write('error')
